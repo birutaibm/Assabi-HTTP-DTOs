5925e4af9f58c0ec76b3c654a777e75d1290103a
Rafael Arantes <birutaibm@gmail.com>
2019-11-04 17:58:21 -0200

    Remove my own implementation of AllArgsConstructor at WeightCreationDTO

--- src/assabi/dto/WeightCreationDTO.java
+++ src/assabi/dto/WeightCreationDTO.java
@@ -1,6 +1,5 @@
 package assabi.dto;
 
-import java.beans.ConstructorProperties;
 import java.util.Collection;
 
 import lombok.Data;
@@ -11,26 +10,10 @@ public class WeightCreationDTO {
 	private final int step;
 	private final Collection<Component> components;
 
-	@ConstructorProperties({"group", "step", "components"})
-	public WeightCreationDTO(long group, int step, Collection<Component> components) {
-		super();
-		this.group = group;
-		this.step = step;
-		this.components = components;
-	}
-
 	@Data
 	public static final class Component {
 		private final long weightableId;
 		private final String weightableType;
 		private final double value;
-		
-		@ConstructorProperties({"weightableId", "weightableType", "value"})
-		public Component(long weightableId, String weightableType, double value) {
-			super();
-			this.weightableId = weightableId;
-			this.weightableType = weightableType;
-			this.value = value;
-		}
 	}
 }
