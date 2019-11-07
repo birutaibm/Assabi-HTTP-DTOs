4e3b1ebb9ef2ea2b5826bc0b7a95384f1934fc91
Rafael Arantes <birutaibm@gmail.com>
2019-11-07 15:57:53 -0200

    Add constructors at DistanceOptionDTO

--- src/assabi/dto/DistanceOptionDTO.java
+++ src/assabi/dto/DistanceOptionDTO.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,10 +18,17 @@ public class DistanceOptionDTO {
 	private double value;
 	private Collection<DistanceDimensionDTO> dimensions = new ArrayList<>();
 	
+	public DistanceOptionDTO(String name, String description, double value) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.value = value;
+	}
+	
 	public void addDimensions(DistanceDimensionDTO dimension) {
 		dimensions.add(dimension);
 	}
-	
+
 	@NoArgsConstructor
 	@Getter
 	@Setter(AccessLevel.PRIVATE)
@@ -28,6 +36,12 @@ public class DistanceOptionDTO {
 		private String name;
 		private double value;
 		private Collection<DistanceCriterionDTO> criteria = new ArrayList<>();
+
+		public DistanceDimensionDTO(String name, double value) {
+			super();
+			this.name = name;
+			this.value = value;
+		}
 		
 		public void addCriteria(DistanceCriterionDTO criterion) {
 			criteria.add(criterion);
@@ -35,6 +49,7 @@ public class DistanceOptionDTO {
 	}
 	
 	@NoArgsConstructor
+	@AllArgsConstructor
 	@Getter
 	@Setter(AccessLevel.PRIVATE)
 	public static final class DistanceCriterionDTO {
